77dbe4fc0f34db1c73db22f6eb801c3bb728a1ea
Jaron-Joel <145657783+Jaron-Joel@users.noreply.github.com>
2024-11-22 08:25:28 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+// eigen code
+// header 
+// currency
+const moneyButton = document.querySelector("header > button");
+const deMoney = document.querySelector("moneyButton");
+
+moneyButton.onclick = toggleMenu;
+function toggleMenu() {
+    deMoney.classList.toggle("toonMenu");
+  }
+//hamburger menu
